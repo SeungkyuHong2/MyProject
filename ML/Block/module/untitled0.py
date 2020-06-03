24454d9d86ece43f1cf6285f193cda6c9d560bdc
SeungkyuHong2 <dnjdzk@naver.com>
2020-06-03 20:02:51 +0900

    "Add Block_Project"

---
+++ ML/Block/module/untitled0.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  3 19:30:47 2020
+
+@author: Seung kyu Hong
+"""
+
+from module.simulator import Simulator
+import numpy as np
+simulator = Simulator()
+
